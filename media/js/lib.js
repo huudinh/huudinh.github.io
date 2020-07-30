565aaf4f7c2c37c095a64407dd41a36d8e6e861f
unknown <dinhndvn@yahoo.com>
2020-07-30 11:50:20 +0700

    Js Framework

--- media/js/lib.js
+++ media/js/lib.js
@@ -116,3 +116,167 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+//Tabs
+
+let tab_navs = document.querySelectorAll("ul.tabs li");
+let tab_links = document.querySelectorAll('ul.tabs li.tab-link');
+// let tab_contents = document.querySelectorAll('.tab-content');
+
+for(let tab_nav of tab_navs){
+    tab_nav.addEventListener('click', (e) => {
+        let tab_id = e.target.getAttribute('data-tab');
+
+        let navParent = e.target.parentElement;
+        let navChildrens = navParent.children;
+
+        for(let navChildren of navChildrens){
+            navChildren.classList.remove('current');
+        }
+
+        // Remove Current tabcontent
+        let navParent2 = e.target.parentElement.parentElement;
+        let navChildrens2 = navParent2.children;
+
+        for(let navChildren2 of navChildrens2){
+            navChildren2.classList.remove('current');
+        }
+        
+        let tab_id_active = document.getElementById(tab_id);
+
+        tab_nav.classList.add('current');
+        tab_id_active.classList.add('current');
+    });
+}
+
+//Tabs Single
+
+const tab_nav_sis = document.querySelectorAll('.tabs .tab');
+
+for(let tab_nav_si of tab_nav_sis ){
+    tab_nav_si.addEventListener('click', (e) => {
+        let navParent = e.target.parentElement;
+        let navChildrens = navParent.children;
+
+        for(let navChildren of navChildrens){
+            navChildren.classList.remove('active');
+        }
+      
+        tab_nav_si.classList.add('active');     
+    });
+}
+
+//Tabs Single IMG
+
+const tab_nav_si_imgs = document.querySelectorAll('.tabs .tab img');
+for(let tab_nav_si_img of tab_nav_si_imgs ){
+    tab_nav_si_img.addEventListener('click', (e) => {
+        let navParent = e.target.parentElement.parentElement;
+        let navChildrens = navParent.children;
+
+        for(let navChildren of navChildrens){
+            navChildren.classList.remove('active');
+        }
+      
+        tab_nav_si_img.classList.add('active');    
+    });
+}
+
+// modal
+let modalButton = document.getElementsByClassName('modal-btn');
+let modals = document.getElementsByClassName('modal');
+let modalClose = document.getElementsByClassName('modal-close');
+let closePic = document.getElementsByClassName('modal-closePic');
+let modalBG = document.getElementsByClassName('modal-bg');
+
+for (let i = 0; i < modalButton.length; i++) {
+    for (let j = 0; j < modals.length; j++) {
+        let modalCheck = function () {
+            let x = modalButton[j].getAttribute('data-modal');
+            let y = modals[j].getAttribute('id');
+            if (x == y) {
+                return true;
+            }
+        }
+
+        //Show Modal
+        modalButton[j].addEventListener("click", () => {
+            if (modalCheck()) {
+                modals[j].style.display = "block";
+            }
+        });
+
+        //Hide Modal
+        let modalHide = function (n) {
+            for (let i = 0; i < n.length; i++) {
+                n[i].addEventListener("click", () => {
+                    if (modalCheck()) {
+                        modals[j].style.display = "none";
+                    }
+                });
+            }
+        }
+
+        modalHide(modalClose);
+        modalHide(modalBG);
+        modalHide(closePic);
+    }
+}
+
+// Video Popup
+
+// Chèn Box Modal
+let container = document.getElementsByClassName('container');
+container[0].insertAdjacentHTML('beforeEnd',
+`<div class="modal modal-clipBox" id="modal-clip">
+    <div class="modal-closePic">&times;</div>
+    <div class="modal-bg"></div>
+    <div class="modal-box modal-box-img animate-zoom">
+        <div class="modal-pic">
+            <iframe id="youtube" width="640" height="350"
+                src="" frameborder="0"
+                allowfullscreen></iframe>
+        </div>
+    </div>
+</div>`
+);
+
+let modalVideos = document.getElementsByClassName('modal-clip');
+for(let modalVideo of modalVideos){
+
+    let modalSrc = modalVideo.getAttribute('data-video');
+
+    modalSrc = `//www.youtube-nocookie.com/embed/${modalSrc}?rel=0&controls=1&autoplay=1&nocookie=true`;
+
+    //Tat Popup Video Button Close
+    let modalClipBoxs = document.getElementsByClassName('modal-clipBox');
+
+    // console.log(modalClipBoxs[0]);
+
+    let youtube = document.getElementById('youtube');
+    modalVideo.addEventListener('click', () => {
+        youtube.setAttribute('src', modalSrc);
+        setTimeout(() => {
+            modalClipBoxs[0].style.display = "block";
+        },200);
+    });
+    
+    for(let item of closePic){
+        for(let modalClipBox of modalClipBoxs){
+            item.addEventListener("click", () => {
+                modalClipBox.style.display = "none";                    
+            });
+        }
+
+    }
+
+    // Tat Popup Video Background
+    for(let item of modalBG){
+        for(let modalClipBox of modalClipBoxs){
+            item.addEventListener("click", () => {
+                modalClipBox.style.display = "none";
+            });
+        }
+
+    }
+}
--- media/js/modal.js
+++ media/js/modal.js
@@ -28,7 +28,6 @@ for (let i = 0; i < modalButton.length; i++) {
                 n[i].addEventListener("click", () => {
                     if (modalCheck()) {
                         modals[j].style.display = "none";
-                        modals[j].stopVideo();
                     }
                 });
             }
@@ -37,5 +36,63 @@ for (let i = 0; i < modalButton.length; i++) {
         modalHide(modalClose);
         modalHide(modalBG);
         modalHide(closePic);
+    }
+}
+
+// Video Popup
+
+// Chèn Box Modal
+let container = document.getElementsByClassName('container');
+container[0].insertAdjacentHTML('beforeEnd',
+`<div class="modal modal-clipBox" id="modal-clip">
+    <div class="modal-closePic">&times;</div>
+    <div class="modal-bg"></div>
+    <div class="modal-box modal-box-img animate-zoom">
+        <div class="modal-pic">
+            <iframe id="youtube" width="640" height="350"
+                src="" frameborder="0"
+                allowfullscreen></iframe>
+        </div>
+    </div>
+</div>`
+);
+
+let modalVideos = document.getElementsByClassName('modal-clip');
+for(let modalVideo of modalVideos){
+
+    let modalSrc = modalVideo.getAttribute('data-video');
+
+    modalSrc = `//www.youtube-nocookie.com/embed/${modalSrc}?rel=0&controls=1&autoplay=1&nocookie=true`;
+
+    //Tat Popup Video Button Close
+    let modalClipBoxs = document.getElementsByClassName('modal-clipBox');
+
+    console.log(modalClipBoxs[0]);
+
+    let youtube = document.getElementById('youtube');
+    modalVideo.addEventListener('click', () => {
+        youtube.setAttribute('src', modalSrc);
+        setTimeout(() => {
+            modalClipBoxs[0].style.display = "block";
+        },200);
+    });
+    
+    for(let item of closePic){
+        for(let modalClipBox of modalClipBoxs){
+            item.addEventListener("click", () => {
+                modalClipBox.style.display = "none";                    
+            });
+        }
+
+    }
+
+    // Tat Popup Video Background
+    for(let item of modalBG){
+        for(let modalClipBox of modalClipBoxs){
+            item.addEventListener("click", () => {
+                modalClipBox.style.display = "none";
+            });
+        }
+
     }
 }
