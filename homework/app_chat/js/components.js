b0d24308e16181b311e29df706e67f8531e64e7e
unknown <dinhndvn@yahoo.com>
2020-11-27 12:12:51 +0700

    add chat app

--- homework/app_chat/js/components.js
+++ homework/app_chat/js/components.js
@@ -11,29 +11,32 @@ components.register = `
                 <form id='register-form'>
                 <div class='name-wrapper'>
                     <div class='input-wrapper'>
-                        <div id="firstName-error"></div>
+                        <div id="firstName-error" class='error'></div>
                         <input class='input' type="text" name="firstName" placeholder="First name" />
                     </div>
                     <div class='input-wrapper'>
-                        <div id="lastName-error"></div>
+                        <div id="lastName-error" class='error'></div>
                         <input class='input' type="text" name="lastName" placeholder="Last name" />
                     </div>
                 </div>
 
                 <div class='input-wrapper'>
-                    <div id="email-error"></div>
+                    <div id="email-error" class='error'></div>
                     <input class='input' type="email" name="email" placeholder="Email" />
                 </div>
 
                 <div class='input-wrapper'>
-                    <div id="password-error"></div>
+                    <div id="password-error" class='error'></div>
                     <input class='input' type="password" name="password" placeholder="Password" />
                 </div>
 
                 <div class='input-wrapper'>
-                    <div id="confirmPassword-error"></div>
+                    <div id="confirmPassword-error" class='error'></div>
                     <input class='input' type="password" name="confirmPassword" placeholder="Confirm password" />
                 </div>
+                
+                <div id='form-error' class='form-error'></div>
+                <div id='form-success' class='form-success'></div>
 
                 <div class='input-wrapper register-footer'>
                     <a id='already-have-account'>Already have an account? Login</a>
@@ -75,4 +78,63 @@ components.login = `
             </div>
         </div>
     </div>
-`; 
+`;
+
+components.chat = `
+    <div class='chat-container'>
+      <div class='header'>
+        SCI Chat
+      </div>
+
+      <div class='main'>
+        <div class='conversation-list' id='conversation-list'>
+          <div class='create-conversation'>
+            <button id='create-conversation' class='btn'> + New Conversation</button>
+          </div>
+        </div>
+        
+        <div class='conversation-detail'>
+          <div id='conversation-name' class='conversation-header'>
+            Welcome to My Chat
+          </div>
+
+          <div class='conversation-messages' id='conversation-messages'>
+          </div>
+
+          <form name='message-form' id='message-form'>
+            <div class='conversation-input'>
+              <input id='message-input' name='message' placeholder='Type a message ...'></input>
+              <button class='button' type='submit'>Send</button>
+            </div>
+          <form>
+        </div>
+      </div>
+    </div>
+`;
+
+components.createConversation = `
+  <div class='create-conversation-container'>
+    <div class='header'>
+      SCI Chat
+    </div>
+
+    <div class='main'>
+      <h3>Create a new conversation</h3>
+      <form id='create-conversation-form' class='conversation-form'>
+        <div class='input-wraper'>
+          <input class='input' id='conversationName' name='conversationName' placeholder='Conversation name'></input>
+          <div id='conversation-name-error' class='error'></div>
+        </div>
+
+        <div class='input-wraper'>
+          <input class='input' id='friendEmail' name='friendEmail' placeholder='Friend email'></input>
+          <div id='friend-email-error' class='error'></div>
+        </div>
+        <div>
+          <button class='btn' type='submit'>Create</button>
+          <button class='btn' id='cancel-create-conversation'>Cancel</button>
+        </div>
+      </form>
+    </div>
+  </div>
+`;
--- homework/app_chat/js/view.js
+++ homework/app_chat/js/view.js
@@ -1,4 +1,9 @@
 const view = {};
+
+view.setMessage = (elementId, message = '') => {
+    document.getElementById(elementId).innerText = message;
+}
+
 view.setActiveScreen = (screenName) => {
     switch (screenName) {
         case 'register':
@@ -6,10 +11,9 @@ view.setActiveScreen = (screenName) => {
             document.getElementById('app').innerHTML = components.register;
 
             // add register button listeners
-            document.getElementById('already-have-account').addEventListener('click', () => {
-                view.setActiveScreen('login');
-            });
+            document.getElementById('already-have-account').addEventListener('click', () => view.setActiveScreen('login'));
 
+            // listen to form submit
             const registerForm = document.getElementById('register-form');
             registerForm.addEventListener('submit', (e) => {
                 e.preventDefault();
@@ -24,7 +28,8 @@ view.setActiveScreen = (screenName) => {
 
                 controller.register(registerInfo);
             });
-            
+            break;
+
         case 'login':
             // mount login screen
             document.getElementById('app').innerHTML = components.login;
@@ -47,15 +52,175 @@ view.setActiveScreen = (screenName) => {
             break;
 
         case 'chat':
+  
             // mount chat screen
-            document.getElementById('app').innerHTML = `
-                  <div>UID: ${model.authUser.uid}</div>
-                  <div>Email: ${model.authUser.email}</div>
-                  <div>Display Name: ${model.authUser.displayName}</div>
-                `;
+            document.getElementById('app').innerHTML = components.chat;
+
+            // add message form listener
+            const messageForm = document.getElementById('message-form');
+            messageForm.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const newMessage = messageForm.message.value;
+                controller.addMessage(newMessage);
+
+                messageForm.message.value = '';
+            });
+
+            // create conversation listener
+            document.getElementById('create-conversation').addEventListener('click', () => view.setActiveScreen('createConversation'));
+
+            // remove notification
+            document.getElementById('message-input').addEventListener('click', () => view.removeNotification(model.activeConversation.id));
+
+            //load conversations
+            model.loadConversations();
+            break;
+
+        case 'createConversation':
+            // mount createConversation screen
+            document.getElementById('app').innerHTML = components.createConversation;
+
+            // listen to "Cancel" button
+            document.getElementById('cancel-create-conversation').addEventListener('click', () => view.backToChatScreen());
+
+            // listen to form submit
+            const createConversationForm = document.getElementById('create-conversation-form');
+            createConversationForm.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const conversationName = createConversationForm.conversationName.value;
+                const friendEmail = createConversationForm.friendEmail.value;
+
+                controller.createConversation({
+                    conversationName,
+                    friendEmail,
+                });
+            });
             break;
     }
 };
-view.setMessage = (elementId, message = '') => {
-    document.getElementById(elementId).innerText = message;
-}
+
+view.addMessage = (messageObject) => {
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('message-container');
+
+    const message = document.createElement('div');
+    message.classList.add('message');
+    message.innerText = messageObject.content;
+
+    if (messageObject.user === model.authUser.email) {
+        messageContainer.classList.add('your');
+    } else {
+        const sender = document.createElement('div');
+        sender.classList.add('sender');
+        sender.innerText = messageObject.user;
+        messageContainer.appendChild(sender);
+    }
+
+    messageContainer.appendChild(message);
+    document.getElementById('conversation-messages').appendChild(messageContainer);
+};
+
+view.addConversation = (conversationObj) => {
+    const conversationName = document.createElement('span');
+    conversationName.innerText = conversationObj.name;
+
+    const conversationNoti = document.createElement('span');
+    conversationNoti.classList.add('notification');
+    conversationNoti.innerText = 'new';
+
+    const conversationContainer = document.createElement('div');
+    conversationContainer.classList.add('conversation');
+    conversationContainer.id = conversationObj.id;
+    conversationContainer.appendChild(conversationName);
+    conversationContainer.appendChild(conversationNoti);
+
+    if (conversationObj.id === model.activeConversation.id){
+        conversationContainer.classList.add('selected-conversation');
+    }
+
+    conversationContainer.addEventListener('click', () => {
+        view.removeNotification(conversationObj.id);
+        controller.changeActiveConversation(conversationObj.id);
+    });
+
+    document.getElementById('conversation-list').appendChild(conversationContainer);
+
+    const mediaQueryResult = window.matchMedia('screen and (max-width: 768px)');
+    if (mediaQueryResult.matches) {
+        const conversationElement = document.getElementById(conversationObj.id);
+        const firstLetter = conversationObj.name.charAt(0).toUpperCase();
+        conversationElement.firstChild.innerText = firstLetter;
+
+        document.getElementById('create-conversation').innerText = '+';
+    }
+
+    // media query listener
+    mediaQueryResult.addListener((mediaQuerry) => {
+        if (mediaQuerry.matches) {
+            const conversationElement = document.getElementById(conversationObj.id);
+            const firstLetter = conversationObj.name.charAt(0).toUpperCase();
+            conversationElement.firstChild.innerText = firstLetter;
+
+            document.getElementById('create-conversation').innerText = '+';
+        } else {
+            const conversationElement = document.getElementById(conversationObj.id);
+            conversationElement.firstChild.innerText = conversationObj.name;
+
+            document.getElementById('create-conversation').innerText = '+ New Conversation';
+        }
+    });
+
+};
+
+view.changeActiveConversation = () => {
+    // change conversation Name
+    document.getElementById('conversation-name').innerText = model.activeConversation.name;
+
+    // change selected conversation style
+    document.querySelector('.selected-conversation').classList.remove('selected-conversation');
+    document.getElementById(model.activeConversation.id).classList.add('selected-conversation');
+
+    // re-render messages
+    document.getElementById('conversation-messages').innerHTML = '';
+    for (let message of model.activeConversation.messages){
+        view.addMessage(message);
+    }
+};
+
+view.backToChatScreen = () => {
+    // mount chat screen
+    document.getElementById('app').innerHTML = components.chat;
+
+    // add message form listener
+    const messageForm = document.getElementById('message-form');
+    messageForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const newMessage = messageForm.message.value;
+        controller.addMessage(newMessage);
+
+        messageForm.message.value = '';
+    });
+
+    // create conversation listener
+    document.getElementById('create-conversation').addEventListener('click', () => view.setActiveScreen('createConversation'));
+
+    for (let conversation of model.conversations){
+        view.addConversation(conversation);
+    }
+    for (let message of model.activeConversation.messages){
+        view.addMessage(message);
+    }
+};
+
+view.showNotification = (conversationId) => {
+    const conversation = document.getElementById(conversationId);
+    conversation.lastChild.style.display = 'inline-block';
+};
+
+view.removeNotification = (conversationId) => {
+    const conversation = document.getElementById(conversationId);
+    conversation.lastChild.style.display = 'none';
+};
