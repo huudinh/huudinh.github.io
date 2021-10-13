a107494867f0e33a62de939317f7c38e8a1a5c5c
unknown <dinhndvn@yahoo.com>
2021-10-13 17:32:48 +0700

    fix carausel

--- homework/app_slide2/js/index.js
+++ homework/app_slide2/js/index.js
@@ -25,64 +25,52 @@ let slider = [
         url: 'images/5.jpg',
         text: '5. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique aperiam laudantium voluptatem totam animi alias ab molestiae possimus harum magnam iste repellendus cum eligendi, omnis tenetur! Adipisci repellat aperiam tempora.'
     },
-]
+    {
+        name: 'Slider 6',
+        url: 'images/6.jpg',
+        text: '6. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique aperiam laudantium voluptatem totam animi alias ab molestiae possimus harum magnam iste repellendus cum eligendi, omnis tenetur! Adipisci repellat aperiam tempora.'
+    },
+];
 // console.log(slider);
 
 let slideIndex = [0, 1];
 let slideId = ["mySlides1", "mySlides2"];
 
 function plusSlides(n, no) {
-    if (n == 1){
-        nextSlides(slideIndex[no] += n, no);
-    } else{
-        prevSlides(slideIndex[no] += n, no);
-    }
+    nextSlides(slideIndex[no] += n, no);
 }
 function nextSlides(n, no) {
     let i;
     let x = document.getElementsByClassName(slideId[no]);
     let img = document.querySelectorAll('.mySlides1 img');
     let text = document.querySelectorAll('.mySlides1 .slider__text');
+    document.getElementsByClassName('prev')[0].removeAttribute("disabled");
+    
     console.log(n);
     console.log(img);
-    if (n < 1) { slideIndex[no] = 3 }
-    if (n < 3){
+
+    if (n > x.length) { slideIndex[no] = 0 }
+    if (n < 1) { 
+        slideIndex[no] = x.length ;
+        slider.sort(function(a, b){return 0.5 - Math.random()});
+    }
+
+    if (n < slider.length - 2){
         for (i = 0; i < x.length; i++) {
             img[i].setAttribute('src', slider[i + n]['url']);
             text[i].innerHTML = slider[i + n]['text'];
             console.log(img[i]);
         }
     } else{
+        slider.sort(function(a, b){return 0.5 - Math.random()});
+        console.log(slider);
         slideIndex[no] = 0;
         alert('Bạn đã xem hết');
         for(let i = 0; i < x.length; i++){
             img[i].setAttribute('src', slider[i]['url']);
-            text[i].innerHTML = slider[i + n]['text'];
         }
+        text[1].innerHTML = slider[1]['text'];
     }
    
 
-}
-function prevSlides(n, no) {
-    let i;
-    let x = document.getElementsByClassName(slideId[no]);
-    let img = document.querySelectorAll('.mySlides1 img');
-    let text = document.querySelectorAll('.mySlides1 .slider__text');
-    // console.log(n);
-    // console.log(img);
-    if (n > -3){
-        for (i = 0; i < x.length; i++) {
-            img[i].setAttribute('src', slider[i - n]['url']);
-            text[i].innerHTML = slider[i - n]['text'];
-            console.log(img[i]);
-        }
-    } 
-    else{
-        slideIndex[no] = 0;
-        alert('Bạn đã xem hết');
-        for(let i = 0; i < x.length; i++){
-            img[i].setAttribute('src', slider[i]['url']);
-            text[i].innerHTML = slider[i + n]['text'];
-        }
-    }
 }
