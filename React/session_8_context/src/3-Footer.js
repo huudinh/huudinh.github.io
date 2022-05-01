7ac43989f6c913ae4800e11ce62b98a37b0b5d06
Triệu Hữu Định <dinhth@Macbooks-iMac.local>
2022-05-01 10:37:03 +0700

    react context

---
+++ React/session_8_context/src/3-Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div className="center border" style={{ height: 50 }}>
+            Footer
+            <select>
+                <option>Light</option>
+                <option>Dark</option>
+            </select>
+        </div>
+    );
+};
+
+export default Footer;
