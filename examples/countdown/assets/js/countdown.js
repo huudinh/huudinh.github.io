9b76642370e8c18ebca701c905e49fd35a14d0eb
huudinh <dinhndvn@yahoo.com>
2023-01-13 13:24:52 +0700

    update

--- examples/countdown/assets/js/countdown.js
+++ examples/countdown/assets/js/countdown.js
@@ -1,20 +1,21 @@
-const numberToArr = (number) => {
-    return String(number).split("").map(num => Number(num));
+const numberToArr = (number, width) => {
+    const numberF = new Array(width + 2 - (number + '0').length).join('0') + number;
+    return String(numberF).split("").map(num => Number(num));
 }
 
-const countDecrease = (timeleft, selector) => {
+const countDecrease = (timeleft, selector, width) => {
     var downloadTimer = setInterval(function() {
         if (timeleft == 1) {
             clearInterval(downloadTimer);
         }
         const number = timeleft - 1;
-        countCard(number, selector)
+        countCard(number, selector, width)
         timeleft -= 1;
     }, 1000);
 }
 
-const countCard = (number, selector) => {
-    const numArr = numberToArr(number)
+const countCard = (number, selector, width) => {
+    const numArr = numberToArr(number, width)
     let spanGroup = ''
     for (let item of numArr) {
         spanGroup += `<span>${item}</span>`
@@ -22,8 +23,8 @@ const countCard = (number, selector) => {
     document.querySelector(selector).innerHTML = spanGroup;
 }
 
-countCard(300, '.count__item1');
-countDecrease(300, '.count__item1');
+countCard(100, '.count__item1', 3);
+countDecrease(100, '.count__item1', 3);
 
-countCard(200, '.count__item2');
-countDecrease(10, '.count__item2');
+countCard(20, '.count__item2', 2);
+countDecrease(10, '.count__item2', 2);
