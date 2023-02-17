049e92b88eb1cf4ffffd23c3eafe85b55c1d8ea2
huudinh <dinhndvn@yahoo.com>
2023-02-17 17:51:03 +0700

    update lib.js

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -143,63 +143,36 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Video Popup
-
-    // Chèn Box Modal
-    let container = document.getElementsByTagName('body');
-    container[0].insertAdjacentHTML('beforeEnd',
-        `<div class="modal modal-clipBox" id="modal-clip">
-    <div class="modal-closePic">&times;</div>
-    <div class="modal-box modal-box-video animate-zoom">
-        <div class="modal-video">
-            <iframe id="youtube" src="" frameborder="0" allowfullscreen></iframe>
-        </div>
-    </div>
-    <div class="modal-bg"></div>
-</div>`
-    );
+    const videoPopupCard = (id) => {
+        return `
+            <div class="modal modal-clipBox" id="modal-clip" style="display:flex">
+                <div class="modal-closePic" id="modal-closePic">&times;</div>
+                <div class="modal-box modal-box-video animate-zoom">
+                    <div class="modal-video">
+                        <iframe id="youtube" src="${id}" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="modal-bg" id="modal-bg"></div>
+            </div>
+        `
+    }
 
     let modalVideos = document.getElementsByClassName('modal-clip');
     for (let modalVideo of modalVideos) {
-
         let modalSrc = modalVideo.getAttribute('data-video');
-
         modalSrc = `//www.youtube-nocookie.com/embed/${modalSrc}?rel=0&controls=1&autoplay=1&nocookie=true&mute=true`;
-
-        //Tat Popup Video Button Close
-        let modalClipBoxs = document.getElementsByClassName('modal-clipBox');
-
-        // console.log(modalClipBoxs[0]);
-
-        let youtube = document.getElementById('youtube');
         modalVideo.addEventListener('click', () => {
-            youtube.setAttribute('src', modalSrc);
-            setTimeout(() => {
-                modalClipBoxs[0].style.display = "flex";
-            }, 200);
+            document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', videoPopupCard(modalSrc));
+
+            document.getElementById('modal-closePic').addEventListener("click", () => {
+                document.getElementById('modal-clip').remove();
+            });
+            
+            document.getElementById('modal-bg').addEventListener("click", () => {
+                document.getElementById('modal-clip').remove();
+            });
         });
-
-        for (let item of closePic) {
-            for (let modalClipBox of modalClipBoxs) {
-                item.addEventListener("click", () => {
-                    modalClipBox.style.display = "none";
-                    youtube.setAttribute('src', '');
-                });
-            }
-
-        }
-
-        // Tat Popup Video Background
-        for (let item of modalBG) {
-            for (let modalClipBox of modalClipBoxs) {
-                item.addEventListener("click", () => {
-                    modalClipBox.style.display = "none";
-                    youtube.setAttribute('src', '');
-                });
-            }
-
-        }
     }
-
 });
 
 // Accrodion Single
