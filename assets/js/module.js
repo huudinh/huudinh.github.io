ab2cba1ffab099418824b8747ceb81fb86f14c02
huudinh <dinhndvn@yahoo.com>
2023-11-03 14:14:45 +0700

    update

--- assets/js/module.js
+++ assets/js/module.js
@@ -49,20 +49,21 @@ function dataSourceTheme(path, tag) {
         }
     }
 }
-
 document.addEventListener("scroll", function() {
     myLazy('img.lazy', 'src');
     myLazy('source.lazy', 'srcset');
     myLazy('.lazy-bg', 'img-bg');
     myLazy('.load', 'loaded');
 });
-lazyShowScreen('img.lazy', 'src')
-lazyShowScreen('source.lazy', 'srcset')
+LazyShowScreen('img.lazy', 'src')
+LazyShowScreen('source.load', 'srcset')
+LazyShowScreen('.load', 'loaded')
 
 // Lazy load
 function myLazy(sec, attr) {
     const section_loads = document.querySelectorAll(sec);
     let winTop = window.innerHeight;
+
     for (let i = 0; i < section_loads.length; i++) {
         let pos_top = section_loads[i].getBoundingClientRect().top;
         let pos_bottom = section_loads[i].getBoundingClientRect().bottom;
@@ -93,34 +94,120 @@ function myLazy(sec, attr) {
                 default:
                     console.log(`Sorry, we are out of ${attr}.`);
             }
+
         }
     }
+
 }
 
-function lazyShowScreen(section, attr, sectionDelete) {
-    const section_loads = document.querySelectorAll(section);
+function LazyShowScreen(sec, attr) {
+    const section_loads = document.querySelectorAll(sec);
     let win_height = screen.height;
-
     for (let i = 0; i < section_loads.length; i++) {
+        let delay = section_loads[i].getAttribute('delay-time') || 0;
+
         if (section_loads[i].getBoundingClientRect().top < win_height) {
-            if (sectionDelete) {
-                section_loads[i].classList.remove(sectionDelete);
-                section_loads[i].classList.add(attr);
-
-            } else if (attr == 'srcset') {
-                section_loads[i].srcset = section_loads[i].dataset.srcset;
-                section_loads[i].classList.remove('lazy');
-
-            } else if (attr == 'src') {
-                section_loads[i].src = section_loads[i].dataset.src;
-                section_loads[i].classList.remove('lazy');
-
-            } else {
-                console.log(`Sorry, we are out of ${attr}.`);
+            switch (attr) {
+                case 'loaded':
+                    setTimeout(() => {
+                        section_loads[i].classList.add('loaded');
+                        section_loads[i].classList.remove('load');
+                    }, delay)
+                    break;
+                case 'src':
+                    section_loads[i].src = section_loads[i].dataset.src;
+                    section_loads[i].classList.remove('lazy');
+                    break;
+                case 'srcset':
+                    section_loads[i].srcset = section_loads[i].dataset.srcset;
+                    section_loads[i].classList.remove('lazy');
+                    break;
+                case 'img-bg':
+                    section_loads[i].classList.remove('lazy-bg');
+                    section_loads[i].classList.add('img-bg');
+                    break;
+                case 'slide':
+                    section_loads[i].classList.remove('slide');
+                    section_loads[i].classList.add('slide');
+                    break;
+                default:
+                    console.log(`Sorry, we are out of ${attr}.`);
             }
         }
     }
 }
+
+// document.addEventListener("scroll", function() {
+//     myLazy('img.lazy', 'src');
+//     myLazy('source.lazy', 'srcset');
+//     myLazy('.lazy-bg', 'img-bg');
+//     myLazy('.load', 'loaded');
+// });
+// lazyShowScreen('img.lazy', 'src')
+// lazyShowScreen('source.lazy', 'srcset')
+
+// // Lazy load
+// function myLazy(sec, attr) {
+//     const section_loads = document.querySelectorAll(sec);
+//     let winTop = window.innerHeight;
+//     for (let i = 0; i < section_loads.length; i++) {
+//         let pos_top = section_loads[i].getBoundingClientRect().top;
+//         let pos_bottom = section_loads[i].getBoundingClientRect().bottom;
+//         let delay = section_loads[i].getAttribute('delay-time');
+//         if (pos_top <= winTop && pos_bottom >= 0) {
+//             switch (attr) {
+//                 case 'src':
+//                     section_loads[i].src = section_loads[i].dataset.src;
+//                     section_loads[i].classList.remove('lazy');
+//                     break;
+//                 case 'srcset':
+//                     section_loads[i].srcset = section_loads[i].dataset.srcset;
+//                     section_loads[i].classList.remove('lazy');
+//                     break;
+//                 case 'img-bg':
+//                     section_loads[i].classList.remove('lazy-bg');
+//                     section_loads[i].classList.add('img-bg');
+//                     break;
+//                 case 'loaded':
+//                     setTimeout(() => {
+//                         section_loads[i].classList.remove('load');
+//                         section_loads[i].classList.add('loaded');
+//                     }, delay)
+//                     break;
+//                 case 'slide':
+//                     section_loads[i].classList.add('slide');
+//                     break;
+//                 default:
+//                     console.log(`Sorry, we are out of ${attr}.`);
+//             }
+//         }
+//     }
+// }
+
+// function lazyShowScreen(section, attr, sectionDelete) {
+//     const section_loads = document.querySelectorAll(section);
+//     let win_height = screen.height;
+
+//     for (let i = 0; i < section_loads.length; i++) {
+//         if (section_loads[i].getBoundingClientRect().top < win_height) {
+//             if (sectionDelete) {
+//                 section_loads[i].classList.remove(sectionDelete);
+//                 section_loads[i].classList.add(attr);
+
+//             } else if (attr == 'srcset') {
+//                 section_loads[i].srcset = section_loads[i].dataset.srcset;
+//                 section_loads[i].classList.remove('lazy');
+
+//             } else if (attr == 'src') {
+//                 section_loads[i].src = section_loads[i].dataset.src;
+//                 section_loads[i].classList.remove('lazy');
+
+//             } else {
+//                 console.log(`Sorry, we are out of ${attr}.`);
+//             }
+//         }
+//     }
+// }
 
 // Ajaxload
 function loadDoc(url, cFunction) {
